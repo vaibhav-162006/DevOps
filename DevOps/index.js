475286633d08b56fbacbdd04f1417bd26175a9de
vaibhav-162006 <vaibhavarora90876@gmail.com>
2026-06-23 11:43:54 +0530

    Delete DevOps directory

--- DevOps/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello DevOps");
-console.log("I'm learning CI/CD pipeline using Github Actions");
--- DevOps/test.js
+++
@@ -1,3 +0,0 @@
-console.log('Starting tests.....');
-setTimeout(() => console.log('Waiting 3 seconds...'),3000);
-console.log('Testing Completed');
